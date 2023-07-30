29b1614876e74f43096b5c46c8729b89135f822a
Barry We1iss <barweiss@cisco.com>
2023-07-29 20:06:14 -0400

    Update README.md and docstrings

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# func_logger
+# func_logger - A logger for functions
+
+## Overview
+
+This Python logging module provides a decorator function called `log_output` that can be used to log the output of any function it decorates. The decorator logs the function call details, including arguments, output, and the file path of the module where the function is defined. The log messages can be customized with different log levels and are useful for monitoring and debugging your Python scripts.
+
+## Installation
+
+To use this logging module, follow these steps:
+
+1. Download the `func_logger.py` file and place it in your project directory. Use git, copy it, download, etc.
+2. Make sure you have Python installed on your system.
+
+## Usage
+
+1. Import the `configure_logging` function, and the `log_output` decorator from the `func_logger` module in your Python script:
+
+    ```python
+    from func_logger import configure_logging, log_output
+    ```
+
+2. Decorate any function that you want to log using the log_output decorator. You can provide a custom log level (optional) as an argument to the decorator. If no log level is provided, it will default to logging.INFO.
+
+    ```python
+
+    configure_logging()
+
+    @log_output(log_level='info')
+    def my_function(arg1, arg2):
+        # Your function code here
+        return some_result
+    ```
+
+3. Now, when you call my_function, the decorator will log the function call details, including arguments and output, along with the file path of the module where my_function is defined.
+
+## Example
+
+Here's an example of how the logging module can be used in your Python script. This will send all output for a function that is set at `warning` or hider to a file called `my_logs` and to the stdout:
+
+```python
+from func_logger import log_output, configure_logging
+
+configure_logging(filename='my_logs', level='warning')
+
+@log_output(log_level='warning')
+def divide_numbers(a, b):
+    return a / b
+
+result = divide_numbers(10, 0)
+```
+
+In this example, the divide_numbers function is decorated with `log_output(log_level='warning')`. The log message will include the log level, function call details, and the file path of the module where divide_numbers is defined.
+
+## Log Levels
+
+This logging module supports different log levels for the log messages. This module is built so you don't need to add any more code to your script than necessary. So you don't need to import the logging module where you don't need too. All log levels values for the attribute of both functions can be used as a string. 
+
+The available log levels are:
+
+* `debug`: Detailed information, typically used for debugging purposes.
+* `info`: General information about the program's operation.
+* `warning`: Warning messages, indicating potential issues but not critical errors.
+* `error`: Error messages, indicating a critical error that might require attention.
+* `critical`: Critical error messages, indicating a severe error that might lead to program termination.
+
+## Contributions
+Contributions to this logging module are welcome! If you find any issues or have suggestions for improvements, feel free to create an issue or a pull request on the repository.
--- func_logger.py
+++ func_logger.py
@@ -1,5 +1,6 @@
 import logging
 
+
 '''
 Add to modules with functions to log:
 
@@ -13,34 +14,72 @@ def func(x,y):
 
 '''
 
-def log_configure_map(level):
 
-    log_level = {
+def log_configure_map(level: str) -> int:
+    """
+    Map a string representation of log level to the corresponding integer value.
 
+    Parameters:
+        level (str): The string representation of the log level.
+            Possible values: 'debug', 'info', 'warning', 'error', 'critical'.
+
+    Returns:
+        int: The integer representation of the log level.
+
+    Raises:
+        ValueError: If the input 'level' is not a valid log level.
+    """
+    log_level_map = {
         "debug": logging.DEBUG,
         "info": logging.INFO,
-        "critical": logging.CRITICAL,
-        "error": logging.ERROR,
         "warning": logging.WARNING,
+        "error": logging.ERROR,
+        "critical": logging.CRITICAL,
     }
 
-    return log_level[level]
+    if level.lower() not in log_level_map:
+        raise ValueError(f"Invalid log level: '{level}'. Valid log levels are 'debug', 'info', 'warning', 'error', or 'critical'.")
 
-def log_output_map(level):
+    return log_level_map[level.lower()]
 
-    log_level = {
+def log_output_map(level: str) -> logging.Level:
+    """
+    Map a string representation of log level to the corresponding logging level object.
 
+    Parameters:
+        level (str): The string representation of the log level.
+            Possible values: 'debug', 'info', 'warning', 'error', 'critical'.
+
+    Returns:
+        logging.Level: The corresponding logging level object.
+
+    Raises:
+        ValueError: If the input 'level' is not a valid log level.
+    """
+    log_level_map = {
         "debug": logging.debug,
         "info": logging.info,
-        "critical": logging.critical,
-        "error": logging.error,
         "warning": logging.warning,
+        "error": logging.error,
+        "critical": logging.critical,
     }
 
-    return log_level[level]
+    if level.lower() not in log_level_map:
+        raise ValueError(f"Invalid log level: '{level}'. Valid log levels are 'debug', 'info', 'warning', 'error', or 'critical'.")
 
-def configure_logging(log_filename="langlogger.log", level='info'):
-    
+    return log_level_map[level.lower()]
+
+
+def configure_logging(log_filename: str = 'func_logger.log',
+                      level: str = 'info') -> None:
+    """
+    Configures logging to log messages both to stdout (console) and a file in the current working directory.
+
+    Parameters:
+        log_filename (str, optional): The filename of the log file. Default is 'func_logger.log'.
+        level (str, optional): Represents the log level used by logging. Default is 'info'.
+            Possible log levels: 'debug', 'info', 'warning', 'error', 'critical'.
+    """
     level = log_configure_map(level)
 
     # Configure the root logger
@@ -65,8 +104,44 @@ def configure_logging(log_filename="langlogger.log", level='info'):
     logger.addHandler(file_handler)
 
 def log_output(level='info'):
+    """
+    Decorator function that logs the output of the decorated function.
+
+    Parameters:
+        log_level (str, optional): The logging level for the log messages. Default is 'info'.
+
+    Returns:
+        function: The wrapped function.
+
+    Returns a new function that acts as a wrapper around the original function
+    to log the function call details before calling the original function.
+
+    This decorator can be used to log the inputs and output of any function
+    it decorates. It is particularly useful for monitoring and debugging purposes.
+    The log messages will include the function name, input arguments, output,
+    and the file path of the module where the function is defined.
+
+    Example:
+        @log_output(log_level=logging.WARNING)
+        def divide_numbers(a, b):
+            return a / b
+    """
     def decorator(func):
         def wrapper(*args, **kwargs):
+            """
+            Wrapper function that logs the function call details and calls the original function.
+
+            Parameters:
+                *args: Positional arguments passed to the decorated function.
+                **kwargs: Keyword arguments passed to the decorated function.
+
+            Returns:
+                Any: The return value of the decorated function.
+
+            The wrapper function logs the function call details, including the
+            input arguments and output, after calling the original function.
+            The log messages will be printed based on the provided log level.
+            """
             log = log_output_map(level)
             result = func(*args, **kwargs)
             file_path = func.__module__.__file__
